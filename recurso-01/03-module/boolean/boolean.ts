8fb3d9a774f49ec2f444fe3d63712d54a9d8b436
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-17 01:46:15 -0300

    code sample 'boolean type'

---
+++ recurso-01/03-module/boolean/boolean.ts
@@ -0,0 +1,18 @@
+/**
+ * file: 03-module/boolean/boolean.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of boolean type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+let admin: boolean = true;
+
+console.log(admin);
+
+let senha = "1234";
+
+let senhaSegura: boolean;
+
+senhaSegura = senha.length >= 8;
+
+console.log(senhaSegura);
