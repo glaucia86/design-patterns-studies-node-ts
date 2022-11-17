ddc064be7b57ed9efa1c7261ffd4723457ae0715
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-17 01:16:19 -0300

    code sample about string type

---
+++ recurso-01/03-module/string/string.ts
@@ -0,0 +1,11 @@
+/**
+ * file: 03-module/type-inference/string.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of type string
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+let empresa = 'HCode';
+console.log(empresa);
+
+
