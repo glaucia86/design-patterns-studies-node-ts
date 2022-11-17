e1da6bcf8cd2675dce6d6156ab9429c293c02b57
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-17 01:31:00 -0300

    code sample about union type

---
+++ recurso-01/03-module/union-types/unionTypes.ts
@@ -0,0 +1,12 @@
+/**
+ * file: 03-module/union-types/unionTypes.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of union types
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+let chavePix: string | number;
+
+chavePix = 'valid_email@email.com'
+
+chavePix = 2199998888;
