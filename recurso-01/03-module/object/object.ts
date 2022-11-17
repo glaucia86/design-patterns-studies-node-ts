c8df5c5367a4345e0c822f30a530eeff7072f3e1
glaucia86 <gllemos@microsoft.com>
2022-11-17 20:56:04 -0300

    code sample object type

---
+++ recurso-01/03-module/object/object.ts
@@ -0,0 +1,14 @@
+/**
+ * file: 03-module/object/object.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of object type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const configuracaoServidor: object = {
+  servidor: "Microsoft Azure",
+  ip: "127.0.0.1",
+  senha: "123456",
+};
+
+console.log(configuracaoServidor);
