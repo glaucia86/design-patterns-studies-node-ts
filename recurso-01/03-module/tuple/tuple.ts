b97cc63fdee9be72bbd7b4d9908bead4e4f308e3
glaucia86 <gllemos@microsoft.com>
2022-11-17 20:49:49 -0300

    code sample tuple usage

---
+++ recurso-01/03-module/tuple/tuple.ts
@@ -0,0 +1,12 @@
+/**
+ * file: 03-module/tuple/tuple.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of tuple type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+let superset: [string, number, string];
+
+superset = ['TypeScript', 2011, 'Anders Hejlsberg'];
+
+console.log(superset);
