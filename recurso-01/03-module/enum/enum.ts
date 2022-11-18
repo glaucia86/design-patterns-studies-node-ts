aa315bfe87cdc39b0a32283a617327e15376c125
glaucia86 <gllemos@microsoft.com>
2022-11-17 21:03:08 -0300

    code sample enum type

---
+++ recurso-01/03-module/enum/enum.ts
@@ -0,0 +1,15 @@
+/**
+ * file: 03-module/enum/enum.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of enum type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const Permission = {
+  Read: 1,
+  Write: 2,
+  Execute: 3,
+};
+
+console.log(Permission.Read);
+
