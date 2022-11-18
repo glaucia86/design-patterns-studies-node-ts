05d8a3d6b0ecd652383d9b8651176389f5736935
glaucia86 <gllemos@microsoft.com>
2022-11-17 21:25:26 -0300

    code sample any type

---
+++ recurso-01/03-module/any/any.ts
@@ -0,0 +1,14 @@
+/**
+ * file: 03-module/any/any.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of any type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+let valor;
+
+valor = 10;
+valor = 'Glaucia Lemos';
+valor = true;
+
+console.log(typeof valor);
