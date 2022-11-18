56c0882e1102f3e5df9a72ced5b3dfe822806c7f
glaucia86 <gllemos@microsoft.com>
2022-11-17 21:45:56 -0300

    code sample void type

---
+++ recurso-01/03-module/never/never.ts
@@ -0,0 +1,7 @@
+/**
+ * file: 03-module/never/never.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of never type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
---
+++ recurso-01/03-module/void/void.ts
@@ -0,0 +1,14 @@
+/**
+ * file: 03-module/void/void.ts
+ * date: 11/17/2022
+ * description: file responsible for the understand of void type
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const meuConsole: void = console.log("Hello World!");
+
+function mostrarFrase(): void {
+  console.log("Hello World!");
+}
+
+mostrarFrase();
