4e27d720b67a2ae1123fa7dad006eb983ab364b9
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 09:53:31 -0300

    code sample how to simplify constructor method usage in TypeScript

---
+++ recurso-01/06-module/getSet.ts
@@ -0,0 +1,8 @@
+/**
+ * file: 06-module/getSet.ts
+ * date: 11/28/2022
+ * description: file responsible for understand how to use Getters and Setters in TypeScript
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export { };
