e2a495d6fd73f3b6322b4409fa2cb2c61d122041
glaucia86 <glaucia_lemos86@hotmail.com>
2022-11-28 10:06:37 -0300

    code sample how to use get and set in Ts

--- recurso-01/06-module/getSet.ts
+++ recurso-01/06-module/getSet.ts
@@ -6,3 +6,26 @@
  */
 
 export { };
+
+class Permissao {
+  constructor(
+    private _nome: string,
+    private _nivel: number,
+  ) { }
+
+  get getNome() {
+    return this._nome.toLocaleUpperCase();
+  }
+
+  set setNome(novoNome: string) {
+    if (novoNome.length < 5) {
+      throw new Error('Nome inválido!');
+    }
+
+    this._nome = novoNome;
+  }
+}
+
+const permissao = new Permissao('Administrador', 1);
+console.log(permissao.setNome);
+
