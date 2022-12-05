76ef8f42a2c256d17293938d78f6c3e9b781d0f7
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-05 15:50:50 -0300

    code sample how to use modules in TypeScript.

--- recurso-01/06-module/staticMethods.ts
+++ recurso-01/06-module/staticMethods.ts
@@ -5,43 +5,7 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-interface IBancoDeDados {
-  ip: string;
-  usuario: string;
-  senha: string;
-  tipoBanco: string;
-}
-
-class BancoDeDados {
-
-  static LOCAL = "127.0.0.1";
-  static TIPO_MYSQL = "MySQL";
-  static TIPO_SQLSERVER = "SQL Server";
-
-  constructor(
-    private ip: string,
-    private usuario: string,
-    private senha: string,
-    private tipoBanco: string,
-  ) { }
-
-  static factory(parametros: IBancoDeDados) {
-
-    if (![
-      BancoDeDados.TIPO_MYSQL,
-      BancoDeDados.TIPO_SQLSERVER
-    ].includes(parametros.tipoBanco)) {
-      throw new Error('Tipo de banco de dados não suportado!');
-    }
-
-    return new BancoDeDados(
-      parametros.ip,
-      parametros.usuario,
-      parametros.senha,
-      parametros.tipoBanco,
-    );
-  }
-}
+import { BancoDeDados } from "../scripts/classes/bancoDeDados";
 
 const conexaoBanco = BancoDeDados.factory({
   tipoBanco: BancoDeDados.TIPO_MYSQL,
---
+++ recurso-01/scripts/interfaces/IBancoDeDados.ts
@@ -0,0 +1,14 @@
+/**
+ * file: scripts/interfaces/IBancoDeDados.ts
+ * date: 12/05/2022
+ * description: file responsible for understand how to use modules in TypeScript
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+
+export interface IBancoDeDados {
+  ip: string;
+  usuario: string;
+  senha: string;
+  tipoBanco: string;
+}
